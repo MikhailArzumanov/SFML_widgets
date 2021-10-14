a7325f78b57f1acc8a08a4cc150c36528130d5c3
Арзуманов Михаил <marzumanov@softrust.ru>
2021-10-14 17:12:35 +0300

    Debug widget was removed, text widget contructor now takes init string

--- init_widgets.cpp
+++ init_widgets.cpp
@@ -7,7 +7,4 @@
 void init_widgets() {
     widgets.push_back(new ImageWidget(sprites[0], point{400, 0}));
     widgets.push_back(new AnimatedClose(point{1216, 0}));
-    auto t_widget = new TextWidget(fonts[0], 23, sf::Color::Cyan, {18,48}, {38,19});
-    t_widget->set_content("test text");
-    widgets.push_back(t_widget);
 }
--- text_widget.hpp
+++ text_widget.hpp
@@ -10,7 +10,7 @@ protected:
 	int	 font_size;
 	sf::Font* font;
 public:
-	TextWidget(sf::Font& font_, int font_size_, sf::Color color, point dims_, point p_);
+	TextWidget(sf::Font& font_, int font_size_, sf::Color color, std::string content_, point dims_, point p_);
 	virtual void draw(point shift);
 	virtual void on_click() {}
 	virtual void set_content(std::string content_);
