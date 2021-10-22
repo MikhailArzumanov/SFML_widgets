d9acc2c0eb3b01a24db2fed0269d5e2177df7959
Арзуманов Михаил <marzumanov@softrust.ru>
2021-10-22 14:38:52 +0300

    initialization of widgets was added

--- menu_widget_constructors.cpp
+++ menu_widget_constructors.cpp
@@ -1,6 +1,14 @@
 #include "menu_widget.hpp"
 
 MenuWidget::MenuWidget() {
+	p = { 0,0 };
+	dims = { 1920,1080 };
+
+
 	main_page	  = new MM_MainPage(this);
 	settings_page = new MM_SettingsPage(this);
+	main_page->set_visibility(true);
+	settings_page->set_visibility(false);
+	children.push_back(main_page);
+	children.push_back(settings_page );
 }
--- mm_main_page_constructors.cpp
+++ mm_main_page_constructors.cpp
@@ -2,6 +2,9 @@
 #include "environment.hpp"
 
 MM_MainPage::MM_MainPage(MenuWidget* parent_){
+	p = { 0,0 };
+	dims = { 1920,1080 };
+
 	parent = parent_;
 	settings_btn = new ButtonWidget(fonts[0], 12, sf::Color::Cyan, "settings",menu_event_mtos,parent, sf::Color::White, { 200,100 }, { 325,313 });
 	children.push_back(settings_btn);
