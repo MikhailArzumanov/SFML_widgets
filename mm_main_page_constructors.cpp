b7da551c339546d0b1fb2d2542be1fd871723a59
Арзуманов Михаил <marzumanov@softrust.ru>
2021-10-26 17:03:27 +0300

    Exit button was added

--- mm_main_page_constructors.cpp
+++ mm_main_page_constructors.cpp
@@ -8,9 +8,12 @@ MM_MainPage::MM_MainPage(Widget* parent_){
 
 	parent = parent_;
 	settings_btn = new ButtonWidget(fonts[0], 12, sf::Color::Cyan, "settings",menu_event_mtos,parent, 
-									sf::Color::White, {230, 120}, {425, 225});
+									sf::Color::White, {230, 93}, {425, 225});
 	children.push_back(settings_btn);
 	credits_btn  = new ButtonWidget(fonts[0], 12, sf::Color::Cyan, "credits", menu_event_mtoc, parent,
-		sf::Color::White, {230, 120}, {425, 375});
+		sf::Color::White, {230, 93}, {425, 325});
 	children.push_back(credits_btn);
+	exit_button  = new ButtonWidget(fonts[0], 12, sf::Color::Cyan, "exit", event_mm_leave, parent,
+		sf::Color::White, { 230, 93 }, { 425, 425 });
+	children.push_back(exit_button);
 }
---
+++ mm_main_page_events.cpp
@@ -0,0 +1,6 @@
+#include "mm_main_page.hpp"
+#include "window.hpp"
+
+void event_mm_leave(Widget* widget) {
+	window.close();
+}
