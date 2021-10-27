fc5002b5f801258b6e25d879814434c7e8dab6ad
Арзуманов Михаил <marzumanov@softrust.ru>
2021-10-27 16:19:55 +0300

    implementations for functions was written

--- slider_widget.hpp
+++ slider_widget.hpp
@@ -2,14 +2,16 @@
 #include "widget.hpp"
 
 class SliderWidget : public Widget {
+	Widget* parent;
 	float percentage;
-	void (*onchange)(Widget*);
+	void (*onchange)(Widget*,float);
 	sf::RectangleShape* the_slider;
 	sf::RectangleShape*	  the_line;
 public:
-	void on_click();
+	float get_precentage() { return percentage };
+	void on_click(point mouse);
 	void draw();
 	SliderWidget();
-	SliderWidget(void (*onchange_)(Widget*), point dims_, point p_);
-	SliderWidget(sf::Color slider_color, sf::Color line_color, void (*onchange_)(Widget*), point dims_, point p_);
+	SliderWidget(Widget* parent_, void (*onchange_)(Widget*), point dims_, point p_);
+	SliderWidget(Widget* parent_, sf::Color slider_color, sf::Color line_color, void (*onchange_)(Widget*), point dims_, point p_);
 };
---
+++ slider_widget_draw.cpp
@@ -0,0 +1,9 @@
+#include "slider_widget.hpp"
+#include "window.hpp"
+
+void SliderWidget::draw() {
+	the_line->setPosition(p.x, p.y + dims.y / 3);
+	the_slider->setPosition(p.x + dims.x * percentage, p.y);
+	window.draw(*the_line);
+	window.draw(*the_slider);
+}
