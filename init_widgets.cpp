83038d4f66460e39c25f38de50223a7345b24cc9
Арзуманов Михаил <marzumanov@softrust.ru>
2021-10-27 16:37:14 +0300

    merge conflict resolving

--- init_widgets.cpp
+++ init_widgets.cpp
@@ -1,21 +1,23 @@
 #include "animated_close.hpp"
 #include "main_programm.hpp"
 #include "button_widget.hpp"
+#include "slider_widget.hpp"
 #include "image_widget.hpp"
 #include "rect_widget.hpp"
 #include "text_widget.hpp"
 #include "environment.hpp"
-#include "menu_widget.hpp"
-#include "widget.hpp"
 
 #include <iostream>
 
-void test_event(Widget* null) {
+void test_event(Widget* nullwidget) {
     std::cout << "Test output\n";
 }
 
 void init_widgets() {
     widgets.push_back(new ImageWidget(sprites[0], point{400, 0}));
     widgets.push_back(new AnimatedClose(point{1216, 0}));
-    widgets.push_back(new MenuWidget());
+    widgets.push_back(new ButtonWidget(fonts[0], 12, sf::Color::Cyan, "test button", test_event, nullptr, 
+                                        sf::Color::White, { 325,313 }, { 12,12 }));
+
+    widgets.push_back(new SliderWidget(nullptr, nullptr, { 230,120 }, { 12,340 }));
 }
