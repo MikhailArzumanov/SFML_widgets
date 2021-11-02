7798305c498ff0ce9c7397ee1eb3aedddd290f4c
Арзуманов Михаил <marzumanov@softrust.ru>
2021-11-02 11:25:48 +0300

    Issue was fixed

--- button_widget_constructors.cpp
+++ button_widget_constructors.cpp
@@ -3,21 +3,15 @@
 ButtonWidget::ButtonWidget(){}
 ButtonWidget::ButtonWidget(sf::Font& font_, int font_size_, sf::Color font_color, std::string content_,
 	void (*onclick_)(Widget*), Widget* parent_, sf::Color rect_color, point dims_, point p_) {
-	point pos = dims_/2.f;
-	text = new TextWidget(font_, font_size_, font_color, content_, { 0,0 }, pos);
-	sf::FloatRect frect = text->getTextRect();
-	pos -= point{frect.width,frect.height}/2.f;
-	text->set_p(pos);
+	p = p_;	dims = dims_;
 
-	rect = new sf::RectangleShape({ dims_.x, dims_.y });
+	rect = new sf::RectangleShape({ dims.x, dims.y });
 	rect->setFillColor(rect_color);
-	p = p_;
-
-	dims = dims_;
-
+	
+	text = new TextWidget(font_, font_size_, font_color, "", { 0,0 }, { 0,0 });
+	set_text(content_);
 	add_child(text);
 
 	onclick = onclick_;
-
 	parent = parent_;
 }
--- button_widget_set_text.cpp
+++ button_widget_set_text.cpp
@@ -2,4 +2,8 @@
 
 void ButtonWidget::set_text(std::string text_str) {
 	text->set_content(text_str);
+	point pos = dims / 2.f;
+	sf::FloatRect frect = text->getTextRect();
+	pos -= point{frect.width,frect.height}/2.f;
+	text->set_p(pos);
 }
