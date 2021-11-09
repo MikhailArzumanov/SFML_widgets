9eebdb01848553993d2664c90c07fca8cbe6c994
Арзуманов Михаил <marzumanov@softrust.ru>
2021-11-09 17:08:43 +0300

    text input widget was formed and debugged

---
+++ text_input_widget_constructors.cpp
@@ -0,0 +1,13 @@
+#include "text_input_widget.hpp"
+
+TextInputWidget::TextInputWidget(sf::Font& font, int font_size, sf::Color background_color, sf::Color text_color, 
+								void (*onchange_)(Widget*,std::string), Widget* parent_, std::string placeholder_, point dims_, point p_) {
+	placeholder = placeholder_; dims = dims_; p = p_; 
+	parent = parent_; value = ""; onchange = onchange_;
+
+	text = new TextWidget(font, font_size, text_color, placeholder, { 0,0 }, { 0,0 });
+	add_child(text);
+
+	background = new sf::RectangleShape({dims.x, dims.y});
+	background->setFillColor(background_color);
+}
