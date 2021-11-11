67b2f818b310432696376fdd5a7f32ece32eef38
Арзуманов Михаил <marzumanov@softrust.ru>
2021-11-11 15:47:12 +0300

    slider widget draw shift was fixed

--- slider_widget_draw.cpp
+++ slider_widget_draw.cpp
@@ -2,8 +2,8 @@
 #include "window.hpp"
 
 void SliderWidget::draw(point shift) {
-	the_line->setPosition(p.x, p.y + dims.y / 3);
-	the_slider->setPosition(p.x + dims.x * percentage, p.y);
+	the_line->setPosition(shift.x+p.x, shift.y+p.y + dims.y / 3);
+	the_slider->setPosition(shift.x+p.x + dims.x * percentage, shift.y+p.y);
 	window.draw(*the_line);
 	window.draw(*the_slider);
 	Widget::draw(shift);
