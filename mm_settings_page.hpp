3d68615655e77f3df4921b8d08697c1f82e8dceb
Арзуманов Михаил <marzumanov@softrust.ru>
2021-11-11 16:06:27 +0300

    small changes

--- mm_settings_page.hpp
+++ mm_settings_page.hpp
@@ -1,9 +1,11 @@
 #pragma once
 #include "widget.hpp"
 #include "button_widget.hpp"
+#include "checkbox_widget.hpp"
 
 class MM_SettingsPage : public Widget {
 	Widget* parent;
+	CheckboxWidget* checkbox;
 	ButtonWidget* main_menu_btn;
 public:
 	void on_click(point mouse) {}
