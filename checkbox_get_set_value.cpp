73470d534f4b20be719e6a25039cdd2d751f6012
Арзуманов Михаил <marzumanov@softrust.ru>
2021-11-11 16:14:44 +0300

    problem was solved

--- checkbox_get_set_value.cpp
+++ checkbox_get_set_value.cpp
@@ -1,4 +1,8 @@
 #include "checkbox_widget.hpp"
 
 bool CheckboxWidget::getValue() { return value; }
-void CheckboxWidget::setValue(bool val) { value = val; on_change(); }
+void CheckboxWidget::setValue(bool val) { 
+	value = val; 
+	if(onchange!=nullptr)
+		onchange(parent,value); 
+}
--- checkbox_widget.hpp
+++ checkbox_widget.hpp
@@ -3,15 +3,16 @@
 
 class CheckboxWidget : public Widget {
 	bool value;
+	Widget* parent;
 	sf::Vertex* left_line;
 	sf::Vertex* right_line;
 	sf::RectangleShape* box;
+	void (*onchange)(Widget*, bool);
 public:
 	bool getValue();
 	void setValue(bool val);
-	virtual void on_change() {}
 	virtual void on_click(point mouse);
 	void draw(point shift);
 	CheckboxWidget();
-	CheckboxWidget(point dims_, point p_);
+	CheckboxWidget(Widget* parent_, void (*on_change)(Widget*, bool), point dims_, point p_);
 };
