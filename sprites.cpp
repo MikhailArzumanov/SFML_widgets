52a6a10a0ee5c19f15b0355b98f8b818a08beeef
Арзуманов Михаил <marzumanov@softrust.ru>
2021-11-15 16:26:08 +0300

    Image button was finished

--- sprites.cpp
+++ sprites.cpp
@@ -3,8 +3,8 @@
 const int sprites_amount = 2;
 
 std::string* textures_paths = new std::string[sprites_amount]{
-    "test.png",
-    "close_animated.png",
+    "textures/test.png",
+    "textures/close_animated.png",
 };
 sf::Texture* textures = new sf::Texture[sprites_amount];
 sf::Sprite*  sprites  = new sf::Sprite[sprites_amount];
