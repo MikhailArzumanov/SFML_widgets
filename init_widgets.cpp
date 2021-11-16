223ccfb96741a24b7ebd7e7fc8b3dd003b8d5d5f
Арзуманов Михаил <marzumanov@softrust.ru>
2021-11-16 13:04:33 +0300

    debug widget was removed

--- init_widgets.cpp
+++ init_widgets.cpp
@@ -1,14 +1,8 @@
 #include "animated_close.hpp"
-#include "main_programm.hpp"
-#include "button_widget.hpp"
-#include "image_widget.hpp"
-#include "rect_widget.hpp"
-#include "text_widget.hpp"
 #include "menu_widget.hpp"
 #include "widgets.hpp"
-#include "widget.hpp"
-#include "test_ddmenu_widget.hpp"
 #include "fonts.hpp"
+
 #include <iostream>
 
 void test_event(Widget* null) {
@@ -18,5 +12,4 @@ void test_event(Widget* null) {
 void init_widgets() {
     widgets.push_back(new AnimatedClose(point{1216, 0}));
     widgets.push_back(new MenuWidget());
-    widgets.push_back(new TestDDMenuWidget(fonts[0], { 380,380 }));
 }
--- test_ddmenu_widget_on_change.cpp
+++
@@ -1,6 +0,0 @@
-#include "test_ddmenu_widget.hpp"
-#include <iostream>
-
-void TestDDMenuWidget::on_change(std::string new_val) {
-	std::cout << new_val << "\n";
-}
