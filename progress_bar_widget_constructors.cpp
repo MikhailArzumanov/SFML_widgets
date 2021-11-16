27a23117c18d8952fdcb7e19439fe053adfd0a14
Арзуманов Михаил <marzumanov@softrust.ru>
2021-11-16 15:12:07 +0300

    ProgressBarWidget's constructor was truncated

--- progress_bar_widget_constructors.cpp
+++ progress_bar_widget_constructors.cpp
@@ -1,8 +1,9 @@
 #include "progress_bar_widget.hpp"
 
 ProgressBarWidget::ProgressBarWidget(){}
-ProgressBarWidget::ProgressBarWidget(sf::Color bground_color, sf::Color bar_color, point paddings_, point dims_, point p_) {
-	paddings = paddings_; dims = dims_; p = p_;
+ProgressBarWidget::ProgressBarWidget(sf::Color bground_color, sf::Color bar_color, point paddings_, 
+	point dims_, point p_):Widget(dims_,p_) {
+	paddings = paddings_;
 	bground = new sf::RectangleShape(sf::Vector2f(dims_.x, dims_.y));
 	bground->setFillColor(bground_color);
 	point bar_size = dims-paddings*2;
